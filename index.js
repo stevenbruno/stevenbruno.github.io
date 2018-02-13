7497040299478ec80a7786f993d4f4f16700a355
Steven Bruno <sbruno636@gmail.com>
2018-02-13 14:50:53 -0600

    renamed pages, restyled homepage, added icons, beginning menu burger

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function dropmenu() {
+	document.getElementById("droplist").classList.toggle("show");
+}
 
+//close dropdown if user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown_content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
